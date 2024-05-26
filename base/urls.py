24aeb8656df5025e1a47f1f4fcdd6c477692832a
Anaik Chacon <chaconanaik@gmail.com>
2024-05-25 23:40:09 -0300

    Added many images and started adding projects and case studies to website

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('projects', views.projects)
+    path('projects', views.projects),
+    path('brand-guide', views.brandguide),
+    path('boba-bar', views.bobabar),
+    path('campsite-companions', views.campsitecompanions)
 ]
--- base/views.py
+++ base/views.py
@@ -7,3 +7,12 @@ def home(request):
 
 def projects(request):
     return render(request, 'base/projects.html')
+
+def brandguide(request):
+    return render(request, 'base/brand-guide.html')
+
+def bobabar(request):
+    return render(request, 'base/boba-bar.html')
+
+def campsitecompanions(request):
+    return render(request, 'base/campsite-companions.html')
