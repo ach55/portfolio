4136c3fff3b602e4e53f70c4392d4850db469ba8
Anaik Chacon <chaconanaik@gmail.com>
2024-09-11 19:36:50 -0300

    Disabled static files dirs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,9 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/images/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles")
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
